15056888c1961745f03e25eade6b1abaa85b8775
liuhui <liuhui-751@163.com>
2013-05-29 01:37:56 +0800

    更新zabbix
    增加内容“如何自定义SNMP发现策略和条目”
    插入图片

--- source/conf.py
+++ source/conf.py
@@ -27,7 +27,7 @@ import sys, os
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx',
                 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath',
-                'sphinx.ext.mathjax', 'sphinx.ext.viewcode']
+                'sphinx.ext.mathjax', 'sphinx.ext.viewcode', 'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
